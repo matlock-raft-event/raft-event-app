a3a169c97a044a277653f5c9d3c018d48c4f7ca4
samhepburn-98 <samhepburn98@msn.com>
2023-04-13 17:10:26 +0100

    chore: allow absolute imports

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,5 @@
 import type { GatsbyConfig } from "gatsby";
+import path from "path";
 
 require("dotenv").config({
     path: `.env.${process.env.NODE_ENV}`,
@@ -13,32 +14,50 @@ const config: GatsbyConfig = {
     // If you use VSCode you can also use the GraphQL plugin
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
-    plugins: [{
-        resolve: "gatsby-source-contentful",
-        options: {
-            "accessToken": process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-            "spaceId": process.env.GATSBY_CONTENTFUL_SPACE_ID
-        }
-    }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-        resolve: "gatsby-plugin-manifest",
-        options: {
-            "icon": "src/images/icon.png"
-        }
-    }, "gatsby-plugin-mdx", {
-        resolve: "gatsby-source-filesystem",
-        options: {
-            "name": "images",
-            "path": "./src/images/"
+    plugins: [
+        { // Allow absolute imports
+            resolve: "gatsby-plugin-root-import",
+            options: {
+                "~": path.resolve(__dirname, "src"),
+            },
         },
-        __key: "images"
-    }, {
-        resolve: "gatsby-source-filesystem",
-        options: {
-            "name": "pages",
-            "path": "./src/pages/"
+        { // Contentful integration plugin
+            resolve: "gatsby-source-contentful",
+            options: {
+                "accessToken": process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+                "spaceId": process.env.GATSBY_CONTENTFUL_SPACE_ID
+            }
         },
-        __key: "pages"
-    }]
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-google-gtag",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-mdx",
+    ]
 };
 
 export default config;
