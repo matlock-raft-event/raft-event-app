983e0766418804ea43df2c643f2ee457b19e99b5
samhepburn-98 <samhepburn98@msn.com>
2023-05-18 14:10:00 +0100

    docs(11): add larger height waves story

--- src/stories/components/Waves.stories.tsx
+++ src/stories/components/Waves.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { YELLOW, GREEN, INFO, WARNING } from "../../theme/palette";
 
 import Waves from "../../components/Waves";
+import { StoryFn } from "@storybook/react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -49,4 +50,13 @@ export const Variant4: Story = {
         topColor: WARNING.main.contrastText,
         bottomColor: WARNING.main.color
     }
+};
+
+export const LargerHeight: Story = {
+    args: {
+        variant: 4,
+        topColor: WARNING.main.contrastText,
+        bottomColor: WARNING.main.color
+    },
+    decorators: [(Story: StoryFn) => <div style={{ height: 500 }}><Story /></div>]
 };
