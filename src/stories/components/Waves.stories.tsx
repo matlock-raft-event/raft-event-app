c947b756c373598898b842f999ad5b5f0a33f808
samhepburn-98 <samhepburn98@msn.com>
2023-05-18 14:16:18 +0100

    fix: fix import linting issues

--- src/stories/components/Waves.stories.tsx
+++ src/stories/components/Waves.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { StoryFn } from "@storybook/react";
 import { YELLOW, GREEN, INFO, WARNING } from "../../theme/palette";
 
 import Waves from "../../components/Waves";
-import { StoryFn } from "@storybook/react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
