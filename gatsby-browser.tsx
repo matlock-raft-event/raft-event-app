2603f65114f03600c3ce15c856dc98228b8c5026
samhepburn-98 <samhepburn98@msn.com>
2023-08-19 23:00:41 +0100

    fix: add font weights to gatsby-browser

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,9 +2,25 @@ import { GatsbyBrowser } from "gatsby";
 
 import GlobalThemeProvider from "~/theme";
 
-import "@fontsource/rokkitt"; // Defaults to weight 400 with normal variant.
-
-import "@fontsource/montserrat/500.css"; // Weight 500.
+import "./index.css";
+import "@fontsource/montserrat/100.css";
+import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/800.css";
+import "@fontsource/montserrat/900.css";
+import "@fontsource/rokkitt/100.css";
+import "@fontsource/rokkitt/200.css";
+import "@fontsource/rokkitt/300.css";
+import "@fontsource/rokkitt/400.css";
+import "@fontsource/rokkitt/500.css";
+import "@fontsource/rokkitt/600.css";
+import "@fontsource/rokkitt/700.css";
+import "@fontsource/rokkitt/800.css";
+import "@fontsource/rokkitt/900.css";
 
 // Wraps the whole app
 // eslint-disable-next-line import/prefer-default-export -- Plugins must export known APIs from their gatsby-browser
