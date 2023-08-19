7f63cd4d2dc73ec2bd4d46d077d1c2760cd31ee4
samhepburn-98 <samhepburn98@msn.com>
2023-08-19 23:18:22 +0100

    fix: update lockfile

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 
-import "../src/index.css";
+import "../index.css";
 import "@fontsource/montserrat/100.css";
 import "@fontsource/montserrat/200.css";
 import "@fontsource/montserrat/300.css";
