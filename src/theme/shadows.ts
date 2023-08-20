49a6148012e3cbbc371983741ac2744acecaef8e
Sam Hepburn <samhepburn98@msn.com>
2023-08-20 20:53:28 +0100

    feat: update theme shadows

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -1,14 +1,33 @@
-import { IShadows } from "./types";
+import { Shadows } from "@mui/material";
 
-const transparent1 = "rgba(145, 158, 171, 0.2)";
-const transparent2 = "rgba(145, 158, 171, 0.14)";
-const transparent3 = "rgba(145, 158, 171, 0.12)";
+const transparent = "rgba(0, 0, 0, 0.25)";
 
-const shadows: IShadows = [
+const shadows: Shadows = [
     "none",
-    `0px 2px 1px -1px ${transparent1},0px 1px 1px 0px ${transparent2},0px 1px 3px 0px ${transparent3}`,
-    `0px 7px 8px -4px ${transparent1},0px 12px 17px 2px ${transparent2},0px 5px 22px 4px ${transparent3}`,
-    `0px 11px 15px -7px ${transparent1},0px 24px 38px 3px ${transparent2},0px 9px 46px 8px ${transparent3}`
+    `1px 1px 0px 0px ${transparent}`,
+    `2px 2px 0px 0px ${transparent}`,
+    `4px 4px 0px 0px ${transparent}`,
+    `5px 5px 0px 0px ${transparent}`,
+    `7px 7px 0px 0px ${transparent}`,
+    `8px 8px 0px 0px ${transparent}`,
+    `10px 10px 0px 0px ${transparent}`,
+    `11px 11px 0px 0px ${transparent}`,
+    `13px 13px 0px 0px ${transparent}`,
+    `14px 14px 0px 0px ${transparent}`,
+    `16px 16px 0px 0px ${transparent}`,
+    `17px 17px 0px 0px ${transparent}`,
+    `19px 19px 0px 0px ${transparent}`,
+    `20px 20px 0px 0px ${transparent}`,
+    `22px 22px 0px 0px ${transparent}`,
+    `23px 23px 0px 0px ${transparent}`,
+    `25px 25px 0px 0px ${transparent}`,
+    `26px 26px 0px 0px ${transparent}`,
+    `28px 28px 0px 0px ${transparent}`,
+    `29px 29px 0px 0px ${transparent}`,
+    `31px 31px 0px 0px ${transparent}`,
+    `32px 32px 0px 0px ${transparent}`,
+    `34px 34px 0px 0px ${transparent}`,
+    `35px 35px 0px 0px ${transparent}`
 ];
 
 export default shadows;
