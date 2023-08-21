e06fa7e60cd73d1b3c88230f92ec8f4420f2688c
Sam Hepburn <samhepburn98@msn.com>
2023-08-21 15:25:39 +0100

    feat: add placeholder hall of fame page

---
+++ src/pages/hall-of-fame.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { PageProps } from "gatsby";
+
+const HallOfFame: React.FC<PageProps> = () => (
+    <main>
+        TODO
+    </main>
+);
+
+export default HallOfFame;
