91d197b1a8ff64c2dd495efea283061c3fc05790
Sam Hepburn <samhepburn98@msn.com>
2023-08-21 15:30:46 +0100

    chore: minor

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,10 +1,11 @@
-import { HeadFC } from "gatsby";
+import * as React from "react";
+import { HeadFC, PageProps } from "gatsby";
 
 import Footer from "~/components/Footer";
 import Waves from "~/components/Waves";
 import SponsorsSection from "~/sections/SponsorsSection";
 
-const Sponsors = () => (
+const Sponsors: React.FC<PageProps> = () => (
     <main>
         <Waves />
         <SponsorsSection />
