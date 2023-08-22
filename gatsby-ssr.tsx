0e45061fa50c89fc75b282e0710849127fbdd20f
Sam Hepburn <samhepburn98@msn.com>
2023-08-22 23:02:54 +0100

    chore: add yet-another-react-lightbox styles

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,6 +2,10 @@ import { GatsbySSR } from "gatsby";
 
 import GlobalThemeProvider from "~/theme";
 
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+
 // Wraps the whole app for SSR
 // eslint-disable-next-line import/prefer-default-export -- Plugins must export known APIs from their gatsby-ssr
 export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => (
