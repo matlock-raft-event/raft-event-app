b3ac17dde984a91ab54fc564e2ee1de37d39c602
Sam Hepburn <samhepburn98@msn.com>
2023-08-22 16:53:03 +0100

    feat: add event route section

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -2,19 +2,25 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import { HeadFC, PageProps } from "gatsby";
 
+import Footer from "~/components/Footer";
 import Waves from "~/components/Waves";
+import EventRouteSection from "~/sections/EventRouteSection";
 import TimingsSection from "~/sections/TimingsSection";
 
 const Info: React.FC<PageProps> = () => {
     const theme = useTheme();
     const primaryColor = theme.palette.primary;
     const secondaryColor = theme.palette.secondary;
+    const darkColor = theme.palette.dark;
 
     return (
         <main>
             <Waves />
-            <TimingsSection />
+            <EventRouteSection />
             <Waves bottomColor={primaryColor.main} topColor={secondaryColor.main} variant={2} />
+            <TimingsSection />
+            <Waves bottomColor={darkColor.main} topColor={primaryColor.main} variant={3} />
+            <Footer />
         </main>
     );
 };
