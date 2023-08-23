968fd71820c53f15b3a63124197885e8e66d2560
Sam Hepburn <samhepburn98@msn.com>
2023-08-23 19:18:00 +0100

    chore: comment out gatsby-plugin-google-gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -58,24 +58,24 @@ const config: GatsbyConfig = {
             },
             __key: "pages"
         },
-        {
-            resolve: "gatsby-plugin-google-gtag",
-            options: {
-                // You can add multiple tracking ids and a pageview event will be fired for all of them.
-                trackingIds: [
-                    process.env.GATSBY_GA_TRACKING_ID // Google Analytics / GA
-                ],
-                // This object is used for configuration specific to this plugin
-                pluginConfig: {
-                    // Puts tracking script in the head instead of the body
-                    head: true,
-                    // Setting this parameter is also optional
-                    respectDNT: true
-                    // Avoids sending pageview hits from custom paths
-                    // exclude: ["/preview/**", "/do-not-track/me/too/"],
-                }
-            }
-        },
+        // {
+        //     resolve: "gatsby-plugin-google-gtag",
+        //     options: {
+        //         // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        //         trackingIds: [
+        //             process.env.GATSBY_GA_TRACKING_ID // Google Analytics / GA
+        //         ],
+        //         // This object is used for configuration specific to this plugin
+        //         pluginConfig: {
+        //             // Puts tracking script in the head instead of the body
+        //             head: true,
+        //             // Setting this parameter is also optional
+        //             respectDNT: true
+        //             // Avoids sending pageview hits from custom paths
+        //             // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        //         }
+        //     }
+        // },
         {
             resolve: "gatsby-plugin-eslint",
             options: {
