a3c15753415afa0a905cbe991af18b427462ee32
Sam Hepburn <samhepburn98@msn.com>
2023-08-23 16:04:18 +0100

    feat: update About page with sections

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,45 +1,23 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
-import { PortableText } from "@portabletext/react";
-import { graphql, useStaticQuery } from "gatsby";
 
 import Footer from "~/components/Footer";
-import Heading from "~/components/Heading";
-import Section from "~/components/Section";
 import Waves from "~/components/Waves";
+import AboutSection from "~/sections/AboutSection";
+import DasacSection from "~/sections/DasacSection";
+import RnliSection from "~/sections/RnliSection";
 
 const About = () => {
     const theme = useTheme();
-
-    const data: Queries.AboutQuery = useStaticQuery(graphql`
-      query About {
-        allSanityAbout {
-         edges {
-          node {
-            _rawContent
-          }
-         }
-      }
-    }
-    `);
-
-    const about = data.allSanityAbout.edges.map(edge => edge.node)[0];
-
     return (
         <main>
             <Waves />
-            <Section backgroundColor={theme.palette.secondary.main}>
-                <Heading
-                    color={theme.palette.secondary}
-                    subtitle="It all started in 1961"
-                    title="What is The Matlock Raft Event?"
-                />
-                {
-                    about._rawContent &&
-                    <PortableText value={about._rawContent as never} />
-                }
-            </Section>
-            <Waves bottomColor={theme.palette.dark.main} topColor={theme.palette.secondary.main} />
+            <AboutSection />
+            <Waves bottomColor={theme.palette.primary.main} topColor={theme.palette.secondary.main} variant={2} />
+            <RnliSection />
+            <Waves bottomColor={theme.palette.secondary.main} topColor={theme.palette.primary.main} variant={3} />
+            <DasacSection />
+            <Waves bottomColor={theme.palette.dark.main} topColor={theme.palette.secondary.main} variant={4} />
             <Footer />
         </main>
     );
