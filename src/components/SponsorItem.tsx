4bf8668ef6e8e74ae1954ffea3b7c0b510c4eb36
Sam Hepburn <samhepburn98@msn.com>
2023-08-27 21:04:48 +0100

    refactor: simplify SponsorItem image logic

--- src/components/SponsorItem.tsx
+++ src/components/SponsorItem.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useEffect, useRef, useState } from "react";
+import React, { CSSProperties, useState } from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 
@@ -28,22 +28,6 @@ const SponsorItem = ({
     readOnly = false
 }: SponsorItemProps) => {
     const theme = useTheme();
-    const ref = useRef<HTMLImageElement>(null);
-
-    const [x, setX] = useState<CSSProperties>();
-
-    // Not ideal but was having nightmarish issues keeping various aspect ratio images inside a 1/1 aspect ratio parent
-    useEffect(() => { // TODO: We now have height and width from Sanity
-        const hasResized = x?.height === "100%";
-        if (hasResized) return;
-
-        const height = ref.current?.getBoundingClientRect().height;
-        const width = ref.current?.getBoundingClientRect().width;
-        const isPortraitImg = height && width && height > width;
-        if (isPortraitImg) {
-            setX({ height: "90%" }); // 100% - 5% padding
-        }
-    }, []);
 
     const [hover, setHover] = useState(false);
     const onMouseEnter = () => setHover(true);
@@ -68,12 +52,9 @@ const SponsorItem = ({
             }}
         >
             <GatsbyImage
-                ref={ref}
                 alt={altText ?? ""}
                 image={img}
-                src={img}
                 style={{
-                    ...x,
                     maxHeight: "100%",
                     maxWidth: "100%"
                 }}
