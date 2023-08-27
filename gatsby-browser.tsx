4044b3db70451e6e4281cb062793c0a76fbc36d5
Sam Hepburn <samhepburn98@msn.com>
2023-08-27 19:02:13 +0100

    chore: add variable fonts

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -7,6 +7,9 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "./index.css";
 
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/rokkitt";
+
 // Wraps the whole app
 // eslint-disable-next-line import/prefer-default-export -- Plugins must export known APIs from their gatsby-browser
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({ element }) => (
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -7,6 +7,9 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "./index.css";
 
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/rokkitt";
+
 // Wraps the whole app for SSR
 // eslint-disable-next-line import/prefer-default-export -- Plugins must export known APIs from their gatsby-ssr
 export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => (
