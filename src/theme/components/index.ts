e5d0b3ed29e50ce12d4ac6babd9f8a0b353d966a
Sam Hepburn <samhepburn98@msn.com>
2023-08-27 19:04:21 +0100

    fix: remove cssBaseline from global overrides

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,13 +1,11 @@
 import { Theme } from "@mui/material/styles";
 
 import Button from "./Button";
-import CssBaseline from "./CssBaseline";
 import Link from "./Link";
 
 export default function ComponentsOverrides(theme: Theme) {
     return Object.assign(
         Button(theme),
-        CssBaseline(theme),
         Link(theme)
     );
 }
