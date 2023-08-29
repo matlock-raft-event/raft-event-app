583e633693b7ee8b40b40153b8cc2cc0d1fb8af9
Sam Hepburn <samhepburn98@msn.com>
2023-08-29 15:13:44 +0100

    feat: add initial Volunteer Page

---
+++ src/pages/volunteer.tsx
@@ -0,0 +1,84 @@
+import { Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+import Footer from "~/components/Footer";
+import Heading from "~/components/Heading";
+import Section from "~/components/Section";
+import Waves from "~/components/Waves";
+
+const Volunteer = () => {
+    const theme = useTheme();
+    return (
+        <main>
+            <Waves />
+            <Section backgroundColor={theme.palette.secondary.main}>
+                <Heading color={theme.palette.secondary} subtitle="How to help out" title="Volunteer" />
+                <Typography variant="h6">
+                    Safety Marshalls
+                </Typography>
+                <Typography variant="body1">
+                    The Matlock Raft Event is a fun event for all the family on Boxing Day. However, the negative
+                    effects on the environment that this level of spectatorship and the active involvement of the
+                    spectators has must be seriously considered.
+                </Typography>
+                <Typography variant="body1">
+                    Spectators have often been known to playfully throw flour and eggs at raft participants in the past.
+                    While this appears to be great fun, it&apos;s important to note that plastic bags can be harmful to
+                    the watercourse and eggs pose risks to our participants. Both the environment agency and council are
+                    closely monitoring the situation, and we&apos;re taking steps to ensure safety and environmental
+                    responsibility.
+                </Typography>
+                <Typography variant="body1">
+                    To keep the event thriving, we&apos;re expanding our team of Marshalls to enhance safety and
+                    enjoyment.
+                </Typography>
+                <Typography variant="body1">
+                    As a safety marshall, you will be responsible to:
+                </Typography>
+                <ul>
+                    <li>
+                        Lorem Ipsum
+                    </li>
+                    <li>
+                        Lorem Ipsum
+                    </li>
+                </ul>
+                <Typography variant="body1">
+                    Join us in ensuring a fantastic experience for everyone at our event. You&apos;ll
+                    receive £35 to cover your expenses.
+                </Typography>
+                <Typography variant="body1">
+                    Contact TODO via for more information.
+                </Typography>
+
+                <br />
+
+                <Typography variant="h6">
+                    Donation Collectors
+                </Typography>
+                <Typography variant="body1">
+                    We’ve already raised over £250,000 for the RNLI. Can you spare us a day to help us collect the
+                    donations from the spectators, families and groups of friends?
+                </Typography>
+                <Typography variant="body1">
+                    As a donation collector, you will be responsible to:
+                </Typography>
+                <ul>
+                    <li>
+                        Lorem Ipsum
+                    </li>
+                    <li>
+                        Lorem Ipsum
+                    </li>
+                </ul>
+                <Typography variant="body1">
+                    Contact TODO via for more information.
+                </Typography>
+            </Section>
+            <Waves bottomColor={theme.palette.dark.main} topColor={theme.palette.secondary.main} variant={2} />
+            <Footer />
+        </main>
+    );
+};
+
+export default Volunteer;
