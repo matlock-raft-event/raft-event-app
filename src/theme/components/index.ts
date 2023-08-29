de8f370d3a87cbac14f256af19a7271306acb0c4
Sam Hepburn <samhepburn98@msn.com>
2023-08-29 15:14:33 +0100

    feat: add custom ToggleButton styles

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -2,10 +2,12 @@ import { Theme } from "@mui/material/styles";
 
 import Button from "./Button";
 import Link from "./Link";
+import ToggleButton from "./ToggleButton";
 
 export default function ComponentsOverrides(theme: Theme) {
     return Object.assign(
         Button(theme),
-        Link(theme)
+        Link(theme),
+        ToggleButton(theme)
     );
 }
