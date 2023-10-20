c288aadcb6164161648a61b0d9a0fd3226b2d062
Sam Hepburn <samhepburn98@msn.com>
2023-10-20 13:03:57 +0100

    chore: sort imports

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,13 +2,13 @@ import { GatsbySSR } from "gatsby";
 
 import GlobalThemeProvider from "~/theme";
 
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/rokkitt";
+
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "./index.css";
-
-import "@fontsource-variable/montserrat";
-import "@fontsource-variable/rokkitt";
 
 // Wraps the whole app for SSR
 // eslint-disable-next-line import/prefer-default-export -- Plugins must export known APIs from their gatsby-ssr
