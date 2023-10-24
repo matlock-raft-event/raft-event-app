36b978d43c10eb699bb04bf2082628f2d2045b05
Sam Hepburn <samhepburn98@msn.com>
2023-10-24 22:36:40 +0100

    fix: blur get involved images before load

--- src/sections/GetInvolvedSection.tsx
+++ src/sections/GetInvolvedSection.tsx
@@ -13,7 +13,7 @@ const GetInvolvedSection = () => {
                 nodes {
                     relativePath
                     childImageSharp {
-                        gatsbyImageData
+                        gatsbyImageData(placeholder: BLURRED)
                     }
                 }
             }
