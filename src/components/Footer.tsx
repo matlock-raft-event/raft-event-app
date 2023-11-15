f6e7eae229c8a66e6b0894cf5b0434d0a1c05076
Sam Hepburn <samhepburn98@msn.com>
2023-11-15 10:18:49 +0000

    feat: update footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
+import * as React from "react";
+import { Stack, Typography } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import { PaletteColor } from "@mui/material/styles/createPalette";
 import { Link } from "gatsby";
+
+import { TITLE_FONT_FAMILY } from "~/theme/typography";
 
 import FacebookIcon from "./FacebookIcon";
 
@@ -13,6 +17,7 @@ const StyledRow = styled("div")(({ theme }) => ({
     display: "flex",
     justifyContent: "center",
     padding: 8,
+    gap: 32,
     alignItems: "center",
     flexDirection: "column",
     [theme.breakpoints.up("sm")]: {
@@ -20,81 +25,125 @@ const StyledRow = styled("div")(({ theme }) => ({
     }
 }));
 
-const StyledLink = styled(Link)(({ theme }) => ({
-    padding: theme.spacing(1)
-}));
-
-const StyledText = styled("p")({
-    margin: 0,
-    padding: "0 16px"
-});
-
-type Url = {
-    label: string;
-    to: string; // NOTE: to must start with http(s)://
-};
-
 type FooterProps = {
     color?: PaletteColor;
 };
+
+const links = [
+    {
+        label: "About",
+        to: "/about"
+    },
+    {
+        label: "Info",
+        to: "/info"
+    },
+    {
+        label: "Sponsors",
+        to: "/sponsors"
+    },
+    {
+        label: "Contact",
+        to: "/contact"
+    },
+    {
+        label: "Gallery",
+        to: "/gallery"
+    }
+];
+const NavLink = ({
+    label,
+    to
+}: { label: string, to: string }) => {
+    const theme = useTheme();
+    return (
+        <Typography
+            key={label}
+            color="secondary.contrastText"
+            component={Link}
+            sx={{
+                transition:
+                    "all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1)",
+                fontFamily: TITLE_FONT_FAMILY,
+                fontWeight: 600,
+                textTransform: "uppercase",
+                textShadow: `0px 2px ${theme.palette.secondary.dark}`,
+                textDecorationColor: "transparent",
+                "&:hover": {
+                    transform: "scale(1.05)",
+                    textDecorationColor: theme.palette.secondary.contrastText,
+                    textShadow: "none"
+                }
+            }}
+            to={to}
+            variant="h6"
+        >
+            {label}
+        </Typography>
+    );
+};
+
 const Footer = ({ color }: FooterProps) => {
     const theme = useTheme();
-
-    const links: Url[] = [
-        {
-            label: "Take Part",
-            to: "/take-part"
-        },
-        {
-            label: "Sponsor Us",
-            to: "/sponsors"
-        },
-        {
-            label: "FAQs",
-            to: "/faqs"
-        },
-        {
-            label: "Cookies Policy",
-            to: "/cookies"
-        }
-    ];
 
     const bgColor = color?.main ?? theme.palette.secondary.main;
     const textColor = color?.contrastText ?? theme.palette.secondary.contrastText;
 
     return (
         <StyledSection style={{ backgroundColor: bgColor }}>
-            <StyledRow>
-                <FacebookIcon color={textColor} href="https://www.facebook.com/matlockraftevent/" />
-            </StyledRow>
-            <StyledRow>
-                {
-                    links.map(link =>
-                        <StyledLink
-                            key={link.label}
-                            style={{
-                                color: textColor,
-                                textDecorationColor: textColor
-                            }}
-                            to={link.to}
-                        >
-                            {link.label}
-                        </StyledLink>
-                    )
-                }
-            </StyledRow>
-            <StyledRow>
-                <StyledText style={{ color: textColor }}>
-                    &#169;
+            <Stack alignItems="center" spacing={2}>
+                <Stack>
+                    <Typography
+                        sx={{ color: textColor }}
+                        variant="overline"
+                    >
+                        FIND US ELSEWHERE
+                    </Typography>
+                    <StyledRow>
+                        <FacebookIcon color={textColor} href="https://www.facebook.com/matlockraftevent/" />
+                    </StyledRow>
+                </Stack>
+
+                <StyledRow>
+                    {
+                        links.map(link => (
+                            <NavLink
+                                key={link.label}
+                                label={link.label}
+                                to={link.to}
+                            />
+                        ))
+                    }
+                </StyledRow>
+                <Typography
+                    sx={{ color: textColor }}
+                    variant="body2"
+                >
+                    &#169; Copyright
                     {" "}
                     {new Date().getFullYear()}
                     {" "}
-                    All Rights Reserved
-                </StyledText>
-                <StyledText style={{ color: textColor }}>
-                    DASAC&apos;S Matlock Raft Event
-                </StyledText>
-            </StyledRow>
+                    -
+                    {" "}
+                    <strong>Dasac’s Matlock Raft Event</strong>
+                    {" "}
+                    - In the aid of the the
+                    Royal National Lifeboats Institute (RNLI)
+                </Typography>
+                <Typography
+                    sx={{ color: textColor }}
+                    variant="body2"
+                >
+                    Website designed and coded with ♥ by
+                    {" "}
+                    <strong>Ian Ryde</strong>
+                    {" "}
+                    and
+                    {" "}
+                    <strong>Sam Hepburn</strong>
+                    .
+                </Typography>
+            </Stack>
         </StyledSection>
     );
 };
