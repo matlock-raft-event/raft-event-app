1c9c3ccd8960ceedd9d7d144a3c40e6d0bbfd444
Sam Hepburn <samhepburn98@msn.com>
2023-11-19 15:14:24 +0000

    chore: update md breakpoint to 960px

--- src/theme/breakpoints.ts
+++ src/theme/breakpoints.ts
@@ -3,7 +3,7 @@ import { IBreakpoints } from "./types";
 const breakpoints: IBreakpoints = {
     xs: "0px",
     sm: "600px",
-    md: "900px",
+    md: "960px",
     lg: "1200px",
     xl: "1536px"
 };
