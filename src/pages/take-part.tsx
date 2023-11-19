16dd8824181cdf609d33785d98a4513314370b4d
Sam Hepburn <samhepburn98@msn.com>
2023-11-19 16:14:06 +0000

    fix: update text width on take part page

--- src/pages/take-part.tsx
+++ src/pages/take-part.tsx
@@ -11,13 +11,12 @@ import TakePartSection from "~/sections/TakePartSection";
 const TakePart: React.FC<PageProps> = () => {
     const theme = useTheme();
     const secondaryColor = theme.palette.secondary;
-    const darkColor = theme.palette.dark;
 
     return (
         <main>
             <InnerHeroSection />
             <TakePartSection />
-            <Waves bottomColor={darkColor.main} topColor={secondaryColor.main} variant={2} />
+            <Waves bottomColor={secondaryColor.main} topColor={secondaryColor.main} variant={2} />
             <Footer />
         </main>
     );
--- src/sections/TakePartSection.tsx
+++ src/sections/TakePartSection.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Container, Divider, Stack, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
 import ApplicationFormPdf from "~/assets/documents/Application-Form.pdf";
@@ -31,22 +31,26 @@ const TakePartSection = () => {
     return (
         <Section bgColor={theme.palette.secondary}>
             <Heading color={theme.palette.secondary} subtitle="So you're brave enough?" title="Take Part" />
-            <Stack alignItems="center" spacing={2}>
-                <Typography variant="body1">
-                    The Raft Event is approximately 3 miles long, the water is cold and in places deep and
-                    turbulent. There is a weir with a descent totalling around 30 meters of rapid white water.
-                    Are you okay with that? Still want to get cold and wet on Boxing Day? Don’t say we didn’t warn you!
-                </Typography>
-                <Typography variant="body1">
-                    Welcome to an elite club of individuals that choose to give something back to a great cause on
-                    Boxing Day: the RNLI. Do it once and we promise you will be hooked!
-                </Typography>
-                <Typography variant="subtitle1">
-                    To take part in the raft event, you need to complete the following steps:
-                </Typography>
-
-                <Container maxWidth="md">
-                    <Stack alignItems="center" divider={<Divider flexItem light />} spacing={2}>
+            <Container maxWidth="md">
+                <Stack alignItems="center" spacing={2}>
+                    <Stack alignItems="center" spacing={1}>
+                        <Typography variant="body1">
+                            The Raft Event is approximately 3 miles long, the water is cold and in places deep and
+                            turbulent. There is a weir with a descent totalling around 30 meters of rapid white water.
+                            Are you okay with that? Still want to get cold and wet on Boxing Day? Don’t say we didn’t
+                            warn
+                            you!
+                        </Typography>
+                        <Typography variant="body1">
+                            Welcome to an elite club of individuals that choose to give something back to a great cause
+                            on
+                            Boxing Day: the RNLI. Do it once and we promise you will be hooked!
+                        </Typography>
+                    </Stack>
+                    <Stack alignItems="center" spacing={2}>
+                        <Typography variant="subtitle1">
+                            To take part in the raft event, you need to complete the following steps:
+                        </Typography>
                         {
                             STEPS.map(item => (
                                 <StepCard
@@ -62,50 +66,50 @@ const TakePartSection = () => {
                             ))
                         }
                     </Stack>
-                </Container>
 
-                <Stack direction="row" py={2} spacing={2}>
-                    <Button
-                        component="a"
-                        download="Raft-Event-Rules"
-                        href={RulesPdf}
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        Rules & Guidelines
-                    </Button>
-                    <Button
-                        component="a"
-                        download="Pre-Event-Safety"
-                        href={SafetyPdf}
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        Safety Brief
-                    </Button>
-                    <Button
-                        component="a"
-                        download="Application-Form"
-                        href={ApplicationFormPdf}
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        Application Form
-                    </Button>
+                    <Stack direction="row" py={2} spacing={2}>
+                        <Button
+                            component="a"
+                            download="Raft-Event-Rules"
+                            href={RulesPdf}
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Rules & Guidelines
+                        </Button>
+                        <Button
+                            component="a"
+                            download="Pre-Event-Safety"
+                            href={SafetyPdf}
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Safety Brief
+                        </Button>
+                        <Button
+                            component="a"
+                            download="Application-Form"
+                            href={ApplicationFormPdf}
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Application Form
+                        </Button>
+                    </Stack>
+
+                    <Typography>
+                        Once ready and your raft has been checked, be prepared to face whatever the Derbyshire Dales,
+                        the River Derwent and thousands of excited spectators have to offer on a typical Boxing Day.
+                        Best of luck!
+                    </Typography>
+                    <Typography>
+                        This event is only possible thanks to the brave drivers and guest participants working hard to
+                        create some stunning rafts for all of the spectators to enjoy. The more fun and excitement on
+                        the day, the deeper people will dig in their pockets and donate to the RNLI. Derwent and
+                        thousands of excited spectators have to offer on a typical Boxing Day. Best of luck!
+                    </Typography>
                 </Stack>
-
-                <Typography>
-                    Once ready and your raft has been checked, be prepared to face whatever the Derbyshire Dales,
-                    the River Derwent and thousands of excited spectators have to offer on a typical Boxing Day.
-                    Best of luck!
-                </Typography>
-                <Typography>
-                    This event is only possible thanks to the brave drivers and guest participants working hard to
-                    create some stunning rafts for all of the spectators to enjoy. The more fun and excitement on
-                    the day, the deeper people will dig in their pockets and donate to the RNLI. Derwent and
-                    thousands of excited spectators have to offer on a typical Boxing Day. Best of luck!
-                </Typography>
-            </Stack>
+            </Container>
         </Section>
     );
 };
