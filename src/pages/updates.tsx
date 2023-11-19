6504ac005c80f69297acfd9be432a41f9afcc9d3
Sam Hepburn <samhepburn98@msn.com>
2023-11-19 16:25:45 +0000

    fix: update wave colors on updates page

--- src/pages/updates.tsx
+++ src/pages/updates.tsx
@@ -11,13 +11,12 @@ import UpdatesSection from "~/sections/UpdatesSection";
 const Updates: React.FC<PageProps> = () => {
     const theme = useTheme();
     const secondaryColor = theme.palette.secondary;
-    const darkColor = theme.palette.dark;
 
     return (
         <main>
             <InnerHeroSection />
             <UpdatesSection />
-            <Waves bottomColor={darkColor.main} topColor={secondaryColor.main} variant={2} />
+            <Waves bottomColor={secondaryColor.main} topColor={secondaryColor.main} variant={2} />
             <Footer />
         </main>
     );
