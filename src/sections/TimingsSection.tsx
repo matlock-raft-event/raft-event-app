2c1f278825559100a09bf238774d3ad4f10b804e
Sam Hepburn <samhepburn98@msn.com>
2024-11-19 02:47:10 +0000

    feat: refactor timings

--- src/sections/TimingsSection.tsx
+++ src/sections/TimingsSection.tsx
@@ -1,35 +1,43 @@
-import { Container, Stack, Typography } from "@mui/material";
+import {
+    Timeline,
+    TimelineConnector,
+    TimelineContent,
+    TimelineDot,
+    TimelineItem,
+    timelineItemClasses,
+    TimelineSeparator
+} from "@mui/lab";
+import { Container, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
 import Heading from "~/components/Heading";
 import Iconify from "~/components/Iconify";
 import Section from "~/components/Section";
-import StepCard from "~/components/StepCard";
 
 const TIMINGS = [
     {
-        time: "07:00am",
+        time: "08:00am",
         description: "After an evening of festivities, the rafters are up bright and early to arrive at Cawdor " +
             "Quarry, register for the event and make final preparations (all nuts tightened)."
     },
     {
-        time: "10:00am",
+        time: "11:00am",
         description: "The rafts set off on their journey down the Derwent"
     },
     {
-        time: "10:30am",
+        time: "11:30am",
         description: "The rafts arrive at Hall Leys Park in Matlock"
     },
     {
-        time: "11:30am",
+        time: "12:30am",
         description: "The rafts arrive at Derwent Gardens in Matlock Bath"
     },
     {
-        time: "12:00pm",
+        time: "13:00pm",
         description: "The rafts arrive at the finish line at Cromford Meadows in Cromford"
     },
     {
-        time: "14:00pm",
+        time: "15:00pm",
         description: "The ceremony takes place at The Fishpond in Matlock Bath to " +
             "announce the winner of this years raft event!"
     }
@@ -54,22 +62,38 @@ const TimingsSection = () => {
             </Container>
 
             <Container maxWidth="md">
-                <Stack spacing={2}>
+                <Timeline
+                    sx={{
+                        [`& .${timelineItemClasses.root}:before`]: {
+                            flex: 0,
+                            padding: 0
+                        }
+                    }}
+                >
                     {
                         TIMINGS.map(item => (
-                            <StepCard
-                                key={item.time}
-                                content={
-                                    <Typography variant="body2">
-                                        {item.description}
+                            <TimelineItem key={item.time}>
+                                <TimelineSeparator>
+                                    <TimelineConnector />
+                                    <TimelineDot sx={{ bgcolor: theme.palette.red.main }}>
+                                        <Iconify color="red.contrastText" icon="ph:clock" />
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent sx={{
+                                    py: "12px",
+                                    px: 2
+                                }}
+                                >
+                                    <Typography component="span" variant="h6">
+                                        {item.time}
                                     </Typography>
-                                }
-                                icon={<Iconify color="secondary.contrastText" icon="ph:clock" width={40} />}
-                                title={item.time}
-                            />
+                                    <Typography>{item.description}</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
                         ))
                     }
-                </Stack>
+                </Timeline>
             </Container>
         </Section>
     );
